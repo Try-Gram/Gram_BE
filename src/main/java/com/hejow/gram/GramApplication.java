d94331cb48aa5443cc9db7798fa99a2186e8d6a1
Hejow <gmlwh124@naver.com>
2023-10-20 23:23:27 +0900

    Init: gram gram

---
+++ src/main/java/com/hejow/gram/GramApplication.java
@@ -0,0 +1,11 @@
+package com.hejow.gram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GramApplication.class, args);
+    }
+}
