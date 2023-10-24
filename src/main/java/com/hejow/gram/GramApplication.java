7b1d46dda68f9c9c124dfaa1fe16ec0c105a5486
Hejow <gmlwh124@naver.com>
2023-10-24 10:31:09 +0900

    Fix: code convention errors

--- src/main/java/com/hejow/gram/GramApplication.java
+++ src/main/java/com/hejow/gram/GramApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GramApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(GramApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GramApplication.class, args);
+	}
 }
--- src/main/java/com/hejow/gram/common/JasyptConfig.java
+++ src/main/java/com/hejow/gram/common/JasyptConfig.java
@@ -1,6 +1,5 @@
 package com.hejow.gram.common;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.iv.NoIvGenerator;
@@ -9,19 +8,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @Configuration
 @EnableEncryptableProperties
 public class JasyptConfig {
 
-    @Value("${jasypt.encryptor.password}")
-    private String password;
+	@Value("${jasypt.encryptor.password}")
+	private String password;
 
-    @Bean("jasyptStringEncryptor")
-    public StringEncryptor stringEncryptor() {
-        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        encryptor.setPassword(password);
-        encryptor.setIvGenerator(new NoIvGenerator());
-        encryptor.setSaltGenerator(new RandomSaltGenerator());
-        return encryptor;
-    }
+	@Bean("jasyptStringEncryptor")
+	public StringEncryptor stringEncryptor() {
+		StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+		encryptor.setPassword(password);
+		encryptor.setIvGenerator(new NoIvGenerator());
+		encryptor.setSaltGenerator(new RandomSaltGenerator());
+		return encryptor;
+	}
 }
