30baa885243d9c3d53881e3ca5c3804774283a96
Hejow <gmlwh124@naver.com>
2023-10-24 12:08:23 +0900

    Fix: ci errors

--- src/test/java/com/hejow/gram/JasyptTest.java
+++ src/test/java/com/hejow/gram/JasyptTest.java
@@ -1,67 +1,65 @@
 package com.hejow.gram;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.jasypt.encryption.pbe.PBEStringCleanablePasswordEncryptor;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.salt.StringFixedSaltGenerator;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 class JasyptTest {
-    @Test
-    void encryptString_Success_EqualsWithDecrypted() {
-        // given
-        String target = "hejow";
-        PBEStringCleanablePasswordEncryptor encryptor = initEncryptor();
+	@Test
+	void encryptString_Success_EqualsWithDecrypted() {
+		// given
+		String target = "hejow";
+		PBEStringCleanablePasswordEncryptor encryptor = initEncryptor();
 
-        // when
-        String encrypted = encryptor.encrypt(target);
+		// when
+		String encrypted = encryptor.encrypt(target);
 
-        // then
-        assertEquals(target, encryptor.decrypt(encrypted)); // true
-    }
+		// then
+		assertEquals(target, encryptor.decrypt(encrypted)); // true
+	}
 
-    @Test
-    void isEncryptedAlwaysSame_Fail_ByDifferentSalt() {
-        // given
-        String target = "john";
-        PBEStringCleanablePasswordEncryptor encryptor = initEncryptor();
+	@Test
+	void isEncryptedAlwaysSame_Fail_ByDifferentSalt() {
+		// given
+		String target = "john";
+		PBEStringCleanablePasswordEncryptor encryptor = initEncryptor();
 
-        // when
-        String encrypted = encryptor.encrypt(target);
+		// when
+		String encrypted = encryptor.encrypt(target);
 
-        // then
-        assertAll(
-                () -> assertNotEquals(encrypted, encryptor.encrypt(target)),
-                () -> assertNotEquals(encrypted, encryptor.encrypt(target)),
-                () -> assertNotEquals(encrypted, encryptor.encrypt(target))
-        );
-    }
+		// then
+		assertAll(
+			() -> assertNotEquals(encrypted, encryptor.encrypt(target)),
+			() -> assertNotEquals(encrypted, encryptor.encrypt(target)),
+			() -> assertNotEquals(encrypted, encryptor.encrypt(target))
+		);
+	}
 
-    @Test
-    void sameEncrypted_Success_WithFixedSaltGenerator() {
-        // given
-        String target = "moon";
-        String salt = "fixedSalt";
-        StandardPBEStringEncryptor encryptor = initEncryptor();
-        encryptor.setSaltGenerator(new StringFixedSaltGenerator(salt));
+	@Test
+	void sameEncrypted_Success_WithFixedSaltGenerator() {
+		// given
+		String target = "moon";
+		String salt = "fixedSalt";
+		StandardPBEStringEncryptor encryptor = initEncryptor();
+		encryptor.setSaltGenerator(new StringFixedSaltGenerator(salt));
 
-        // when
-        String encrypted = encryptor.encrypt(target);
+		// when
+		String encrypted = encryptor.encrypt(target);
 
-        // then
-        assertAll(
-                () -> assertEquals(encrypted, encryptor.encrypt(target)),
-                () -> assertEquals(encrypted, encryptor.encrypt(target)),
-                () -> assertEquals(encrypted, encryptor.encrypt(target))
-        );
-    }
+		// then
+		assertAll(
+			() -> assertEquals(encrypted, encryptor.encrypt(target)),
+			() -> assertEquals(encrypted, encryptor.encrypt(target)),
+			() -> assertEquals(encrypted, encryptor.encrypt(target))
+		);
+	}
 
-    private StandardPBEStringEncryptor initEncryptor() {
-        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        encryptor.setPassword("password");
-        return encryptor;
-    }
+	private StandardPBEStringEncryptor initEncryptor() {
+		StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+		encryptor.setPassword("password");
+		return encryptor;
+	}
 }
